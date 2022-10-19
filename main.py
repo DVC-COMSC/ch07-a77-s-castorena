1f6ff513dad256c0d60cbafc35f7b24ae8c7a1b4
sarah castorena <ceanz@yahoo.com>
2022-10-19 13:53:08 -0700

    new

--- main.py
+++ main.py
@@ -6,10 +6,18 @@ num2 = list(map(int, input('Enter second list: ').split()))
 # Make your Code
 # ******************************
 num3 = []
-for i in range(len(num1)):
-    num3.append(num1[i])
-    num3.append(num2[i])
-for i in range(len(num1),len(num2)):
-    num3.append(num2[i])
+if(len(num2) > len(num1)):
+    for i in range(len(num1)):
+        num3.append(num1[i])
+        num3.append(num2[i])
+    for i in range(len(num2), len(num1)):
+        num3.append(num2[i]) 
+if(len(num2) < len(num1)):
+    for i in range(len(num2)):
+        num3.append(num1[i])
+        num3.append(num2[i])
+    for i in range(len(num1), len(num2)):
+        num3.append(num1[i])            
+ 
 
 print (num3) 
