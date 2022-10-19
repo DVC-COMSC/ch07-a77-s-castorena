541a960ca8a59560b66dc696b10f6ea99fa93858
sarah castorena <ceanz@yahoo.com>
2022-10-19 13:46:22 -0700

    new

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 
-num1 = list(map(int, input().split()))
-num2 = list(map(int, input().split()))
+num1 = list(map(int, input("Enter the first list: ").split()))
+num2 = list(map(int, input('Enter second list: ').split()))
 
 # ******************************
 # Make your Code
 # ******************************
+num3 = []
+for i in range(len(num1)):
+    num3.append(num1[i])
+    num3.append(num2[i])
+for i in range(len(num1),len(num2)):
+    num3.append(num2[i])
 
-# print (num3) 
+print (num3) 
